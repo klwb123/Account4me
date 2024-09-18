ee6b5788788372f06e5f7bbf87e668ae5dc27065
klwb123 <eevkt12@gmail.com>
2024-09-18 04:53:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(() => console.log("I'm Ready To Work..! 24H"));
 app.get('/', (req, res) => {
   res.send(`
   <body>
-  <center><h1>Bot 24H ON!</h1></center
+  <center><h1>السيد مقتدى جاهز للعمل moqtda on top</h1></center
   </body>`)
 });
 client.on('ready', async () => {
